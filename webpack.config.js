6d56bb4ec26301b5599049bf6cc870bf512d004f
kim-0617 <kkk5993@naver.com>
2023-02-16 16:21:11 +0900

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   name: "portfolio-setting",
+
   mode: "development",
+
   devtool: "eval",
+
   resolve: {
     extensions: [".jsx", ".js", ".json"],
   },
